0239d99a1d7499cdacd14f7cb2eed72928ecab4a
zarlex <alex.zarges@online.de>
2017-04-20 16:55:06 +0200

    add debug output

--- json-server/scripts/run.sh
+++ json-server/scripts/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
+ echo "DEPLOYMENT GROUP NAME ${DEPLOYMENT_GROUP_NAME}"
  export NODE_ENV=$DEPLOYMENT_GROUP_NAME
 fi
-
+whoami
 cd ~/visualisation/app
 /usr/local/bin/pm2 start json-server.js -n www -i 0
