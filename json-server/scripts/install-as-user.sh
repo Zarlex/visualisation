a7fa8823c984dce5566ccb2895e894996f226dac
zarlex <alex.zarges@online.de>
2017-04-22 13:45:43 +0200

    update scripts

--- json-server/scripts/install-as-user.sh
+++ json-server/scripts/install-as-user.sh
@@ -5,11 +5,6 @@ set -e
 echo "installing NVM"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
-# setup nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-source "$NVM_DIR/nvm.sh"
-
 # save nvm env in bash rc
 hasNvmEnv=`grep "export NVM_DIR" ~/.bashrc | cat`
 if [ -z "$hasNvmEnv" ]; then
@@ -32,19 +27,21 @@ cd ~/visualisation
 npm install
 
 # setup NODE_ENV
-#if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
-#    export NODE_ENV=$DEPLOYMENT_GROUP_NAME
-#
-#    hasEnv=`grep "export NODE_ENV" ~/.bash_profile | cat`
-#    if [ -z "$hasEnv" ]; then
-#        echo "export NODE_ENV=$DEPLOYMENT_GROUP_NAME" >> ~/.bash_profile
-#    else
-#        sed -i "/export NODE_ENV=\b/c\export NODE_ENV=$DEPLOYMENT_GROUP_NAME" ~/.bash_profile
-#    fi
-#fi
+if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
+    export NODE_ENV=$DEPLOYMENT_GROUP_NAME
+
+    hasEnv=`grep "export NODE_ENV" ~/.bashrc | cat`
+    if [ -z "$hasEnv" ]; then
+        echo "export NODE_ENV=$DEPLOYMENT_GROUP_NAME" >> ~/.bashrc
+    else
+        sed -i "/export NODE_ENV=\b/c\export NODE_ENV=$DEPLOYMENT_GROUP_NAME" ~/.bashrc
+    fi
+fi
 
 # add node to startup
 hasRc=`grep "su -l $USER" /etc/rc.local | cat`
 if [ -z "$hasRc" ]; then
     sudo sh -c "echo 'su -l $USER -c \"cd ~/visualisation;sh ./run.sh\"' >> //etc/rc.local"
 fi
+
+source ~/.bashrc
