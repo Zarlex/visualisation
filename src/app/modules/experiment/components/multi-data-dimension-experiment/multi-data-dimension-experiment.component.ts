96ad9710ff230703fbe3c22c7d047298e0c0815b
zarlex <alex.zarges@online.de>
2017-06-16 15:44:36 +0200

    add bunch of stuff

--- src/app/modules/experiment/components/multi-data-dimension-experiment/multi-data-dimension-experiment.component.ts
+++ src/app/modules/experiment/components/multi-data-dimension-experiment/multi-data-dimension-experiment.component.ts
@@ -5,6 +5,7 @@ import {VisualisationCollection} from '../../../main/collections/visualisation.c
 import {VisualisationModel} from '../../../main/models/visualisation.model';
 import {RadarChartModel} from '../../../shared/models/radar-chart.model';
 import {MultiDataExperimentItemModel} from '../../models/multi-data-experiment-item.model';
+let dataItems = require('../../collections/multi-data-experiment-items.json');
 
 @Component({
   selector: 'multi-data-dimension-experiment',
@@ -14,18 +15,27 @@ import {MultiDataExperimentItemModel} from '../../models/multi-data-experiment-i
 })
 
 export class MultiDataDimensionExperimentComponent implements OnInit {
+  public manufacturers: VisualisationCollection;
+  public minYear: number;
+  public maxYear: number;
+  public yearFilter: number;
+  public radarChartLabels: Array<String> = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration'];
+
   constructor(public multiDataExperimentItems: MultiDataExperimentItems, public radarChartCollection: RadarChartCollection) {
   }
 
-  public viewModel = {
-    yearFilter: 70
-  };
-
-  public radarChartLabels: Array<String> = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration'];
 
   private setRadarChartCollection(collection: MultiDataExperimentItems, yearFilter: number) {
     this.radarChartCollection.reset();
-    collection.where({modelYear: yearFilter}).forEach((item: MultiDataExperimentItemModel) => {
+    let items: Array<MultiDataExperimentItemModel>;
+    if (yearFilter) {
+      items = <Array<MultiDataExperimentItemModel>>collection.where({
+        modelYear: yearFilter,
+      });
+    } else {
+      items = <Array<MultiDataExperimentItemModel>>collection.toArray();
+    }
+    items.forEach((item: MultiDataExperimentItemModel) => {
       let radarChartModel = new RadarChartModel({
         color: item.getColor(),
         data: [
@@ -42,28 +52,45 @@ export class MultiDataDimensionExperimentComponent implements OnInit {
   }
 
   public filterByYear(val: number) {
+    this.yearFilter = val;
+    this.multiDataExperimentItems.selectable.unSelectAll();
     this.setRadarChartCollection(this.multiDataExperimentItems, val);
   }
 
-  public getColorForOrigin(origin: string){
+  public getColorForOrigin(origin: string) {
     let originRes = <MultiDataExperimentItemModel>this.multiDataExperimentItems.findWhere({origin: origin});
-    if(originRes){
+    if (originRes) {
       return originRes.getColor();
     }
   }
 
-  ngOnInit(): void {
-    this.setRadarChartCollection(this.multiDataExperimentItems, this.viewModel.yearFilter);
+  public toggleFilterByYear(){
+    if(this.yearFilter){
+      this.yearFilter = null;
+    } else {
+      this.yearFilter = this.minYear;
+    }
+  }
 
+  ngOnInit(): void {
+    dataItems.forEach((item: any) => {
+      this.multiDataExperimentItems.add(new this.multiDataExperimentItems.model(item, {parse: true}))
+    });
+    this.manufacturers = this.multiDataExperimentItems.getManufacturers();
+    this.minYear = this.multiDataExperimentItems.getMin('modelYear');
+    this.maxYear = this.multiDataExperimentItems.getMax('modelYear');
+    //this.yearFilter = this.minYear;
+
+    this.setRadarChartCollection(this.multiDataExperimentItems, this.yearFilter);
     this.multiDataExperimentItems.selectable.on('change:add', () => {
-      if(this.multiDataExperimentItems.selectable.getSelected().length>0){
+      if (this.multiDataExperimentItems.selectable.getSelected().length > 0) {
         let selected: MultiDataExperimentItems = new MultiDataExperimentItems();
+        selected.maxCache = this.multiDataExperimentItems.maxCache;
         selected.reset(this.multiDataExperimentItems.selectable.getSelected().toJSON());
-        this.setRadarChartCollection(selected, this.viewModel.yearFilter);
+        this.setRadarChartCollection(selected, this.yearFilter);
       } else {
-        this.setRadarChartCollection(this.multiDataExperimentItems, this.viewModel.yearFilter);
+        this.setRadarChartCollection(this.multiDataExperimentItems, this.yearFilter);
       }
     });
   }
-
 }
