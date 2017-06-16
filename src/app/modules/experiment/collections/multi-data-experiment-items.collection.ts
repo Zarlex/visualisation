ec49175a8cdd048c2741e18fb7a899aece5c8590
zarlex <alex.zarges@online.de>
2017-06-16 15:41:49 +0200

    provide get min function and getManufacturers

--- src/app/modules/experiment/collections/multi-data-experiment-items.collection.ts
+++ src/app/modules/experiment/collections/multi-data-experiment-items.collection.ts
@@ -1,30 +1,41 @@
 import {Injectable} from '@angular/core';
 import {VisualisationCollection} from '../../main/collections/visualisation.collection';
 import {MultiDataExperimentItemModel} from '../models/multi-data-experiment-item.model';
-let dataItems = require( './multi-data-experiment-items.json');
+import {VisualisationModel} from '../../main/models/visualisation.model';
 
 @Injectable()
 export class MultiDataExperimentItems extends VisualisationCollection {
-  private _maxCache = {};
+  public maxCache = {};
+  public minCache = {};
   model = MultiDataExperimentItemModel;
 
-  constructor(){
+  constructor() {
     super();
-    console.log('INIT')
   }
 
-  initialize(){
-    dataItems.forEach((item: any)=>{
-      this.add(new MultiDataExperimentItemModel(item, {parse: true}));
-    });
-  }
-
-  getMax(attr: string){
-    if(!this._maxCache[attr]){
-      this._maxCache[attr] = this.max((model: MultiDataExperimentItemModel)=>{
+  getMin(attr: string) {
+    if (!this.minCache[attr]) {
+      this.minCache[attr] = this.min((model: MultiDataExperimentItemModel) => {
         return model.get(attr);
       }).get(attr);
     }
-    return this._maxCache[attr];
+    return this.minCache[attr];
+  }
+
+  getMax(attr: string) {
+    if (!this.maxCache[attr]) {
+      this.maxCache[attr] = this.max((model: MultiDataExperimentItemModel) => {
+        return model.get(attr);
+      }).get(attr);
+    }
+    return this.maxCache[attr];
+  }
+
+  getManufacturers(): VisualisationCollection {
+    let manufacturers = new VisualisationCollection();
+    this.each((item: MultiDataExperimentItemModel) => {
+      manufacturers.add({id: item.get('manufacturer')})
+    });
+    return manufacturers;
   }
 }
